9132393b63a7d4a1cc8fe57724665cb65b2a8cab
levzagatov <64085873+levzagatov@users.noreply.github.com>
2020-04-25 13:21:54 +0300

    ru.sh & en.sh

--- ru.sh
+++ ru.sh
@@ -1,5 +1,5 @@
 date +"%d %B %Y-го года в %T" >> update-log
-zenity --info --title="Debupdater" -- text="$USER, приветствуем вас в Debupdater! Сейчас мы обновим вашу систему и пакеты." --ok-label="Обновить систему"
+zenity --info --title="Debupdater" --text="$USER, приветствуем вас в Debupdater! Сейчас мы обновим вашу систему и пакеты." --ok-label="Обновить систему"
 apt update >> update-log && apt-get update >> update-log #updating dbs 
 apt-get upgrade -y >> update-log && apt upgrade -y >> update-log #updating progs
 apt autoremove -y  >> update-log
