a23c31ff13a8b73a9581a1b32bc860b64c3b4780
levzagatov <64085873+levzagatov@users.noreply.github.com>
2020-04-26 11:24:43 +0300

    Add files via upload

--- ru.sh
+++ ru.sh
@@ -1,7 +1,8 @@
 date +"%d %B %Y-го года в %T" >> update-log
 zenity --info --title="Debupdater" --text="$USER, приветствуем вас в Debupdater! Сейчас мы обновим вашу систему и пакеты." --ok-label="Обновить систему"
-apt update >> update-log && apt-get update >> update-log #updating dbs 
-apt-get upgrade -y >> update-log && apt upgrade -y >> update-log #updating progs
-apt autoremove -y  >> update-log
-echo "-------------------------------" >> update-log
+apt update >> ~/update-log && apt-get update >> ~/update-log #updating dbs 
+apt-get upgrade -y >> ~/update-log && apt upgrade -y >> ~/update-log #updating progs
+apt full-upgrade -y >> ~/update-log
+apt autoremove -y >> ~/update-log
+echo "-------------------------------" >> ~/update-log
 zenity --info --title="Debupdater" --text="Обновления выполнены успешно." --ok-label="Спасибо"
